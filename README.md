a470383dfebb086b1b494056c13149c03af252e7
durandnico <durandnico@cy-tech.fr>
2024-11-27 21:43:54 +0100

    creation of the sql table

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # architecture-microservices
+
+## setup docker
+
+run the command :
+```sh
+docker compose up -d
+```
+
+## BDD
+
+### setup pgadmin
+
+go to (pgadmin)[http://localhost:15432]
+
+>>> add server
+>>> hostname : postgres_AM (name of the docker)
+>>> domaine : NoName (name of the DB)
+>>> username : ...
+>>> password : ...
+
---
+++ bdd/sql/schema.sql
@@ -0,0 +1,22 @@
+
+DROP DATABASE IF EXISTS pgtest;
+CREATE DATABASE pgtest;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+\c pgtest;
+
+-- Create the producer table with SERIAL id
+CREATE TABLE producer (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Create the coords_gps table with foreign key
+CREATE TABLE coords_gps (
+    producer_id INTEGER NOT NULL,  -- Foreign key referencing producer(id)
+    latitude DECIMAL(9,6) NOT NULL,
+    longitude DECIMAL(9,6) NOT NULL,
+    CONSTRAINT coords_gps_latitude_check CHECK (latitude >= -90 AND latitude <= 90),
+    CONSTRAINT coords_gps_longitude_check CHECK (longitude >= -180 AND longitude <= 180),
+    CONSTRAINT fk_id FOREIGN KEY (producer_id) REFERENCES producer(id)  -- Foreign key constraint
+);
