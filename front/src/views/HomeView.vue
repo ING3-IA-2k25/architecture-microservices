6e631d7f18ca33ec129ad856e550048aa2377b2a
durandnico <durandnico@cy-tech.fr>
2024-11-28 09:31:42 +0100

    (bug fix): repair leaflet bug, sometime it does not show the map at the loading

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -2,6 +2,7 @@
 /* eslint-disable*/
 import { onMounted, ref } from 'vue';
 import "leaflet/dist/leaflet.css";
+import L from 'leaflet'
 import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
 
 const zoom = ref(12)
@@ -17,7 +18,7 @@ const center = ref([43.299999, -0.370000])
         <!-- div pour la map center -->
         <div class="absolute p-15! h-full w-full">
           <div class="bg-ctp-latte-mauve h-full w-full" ref="mapDiv">
-            <l-map ref="map" v-model:zoom="zoom" :center="center">
+            <l-map ref="map" v-model:zoom="zoom" :center="center" :use-global-leaflet="false">
             <l-tile-layer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               layer-type="base"
