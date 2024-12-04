f1400f25fedda630c49c0631600b8afbb0c3dec6
durandnico <durandnico@cy-tech.fr>
2024-12-04 12:05:40 +0100

    (debug): add debug endpoints

--- api/app/debugEndpoints.py
+++ api/app/debugEndpoints.py
@@ -5,6 +5,11 @@ from pydantic import BaseModel
 class ProducerInput(BaseModel):
     name: str
 
+class CoordsInput(BaseModel):
+    producer_id: int
+    uid: int
+    latitude: float
+    longitude: float
 
 from app.bdd import query
 
@@ -14,3 +19,6 @@ router = APIRouter()
 async def get_producers(producer: ProducerInput):
     return query.create_producer(name=producer.name)
 
+@router.post("/coords")
+async def get_coords(coords: CoordsInput):
+    return query.create_coords(producer_id=coords.producer_id, uid=coords.uid, latitude=coords.latitude, longitude=coords.longitude)
