eeca33969717bd22d032ccfa3627bd788bf3f566
durandnico <durandnico@cy-tech.fr>
2024-12-26 21:19:47 +0100

    add: Websocket manager to transmit between kafka consumer & front

--- api/app/websocket.py
+++ api/app/websocket.py
@@ -1,20 +1,79 @@
 from fastapi import FastAPI, WebSocket, APIRouter
 from app.bdd import query
+import asyncio
 
 router = APIRouter()
 
+class SockerManager:
+    def __init__(self):
+        self.consumer_websockets = []
+        self.front_websockets = []
+
+    async def connect_consumer(self, websocket: WebSocket):
+        await websocket.accept()
+        self.consumer_websockets.append(websocket)
+        return websocket
+
+    async def connect_front(self, websocket: WebSocket):
+        await websocket.accept()
+        self.front_websockets.append(websocket)
+
+
+
+
+    def disconnect(self, websocket: WebSocket):
+        if websocket in self.consumer_websockets:
+            self.consumer_websockets.remove(websocket)
+            print("Consumer websocket disconnected")
+            return
+    
+        if websocket in self.front_websockets:
+            self.front_websockets.remove(websocket)
+            print("Front websocket disconnected")
+            return
+
+        raise Exception("Websocket not found")
+
+
+
+    async def broadcast_to_front(self, message: str):
+        err = None
+        
+        if len(self.front_websockets) != 0:
+            print(f"Broadcasting to {len(self.front_websockets)} front websockets")
+
+        for connection in self.front_websockets:
+            try:
+                await connection.send_text(message)
+            except Exception as e:
+                err = str(e)
+                continue
+
+        return err
+
+
+
+    async def receive_from_consumer(self, websocket: WebSocket):
+        data = await websocket.receive_text()
+        await self.broadcast_to_front(data)
+
+
+manager = SockerManager()
+
 # create socket for kafka
-@router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    print("Connected to websocket")
-    while True:
+@router.websocket("/ws/consumer")
+async def websocket_endpoint_consumer(websocket: WebSocket):
+    await manager.connect_consumer(websocket)
+    print("WS/consumer : Connected to websocket")
+    
+    tasks = []
+    while websocket.client_state != 3:
         # get data from kafka
         try:
             data = await websocket.receive_text()
         
             # print data 
-            print(f"Received data: {data}")
+            print(f"DEBUG : Received data: {data}")
             uid, lat, lon = data.split(",")
             lat = float(lat)
             lon = float(lon)
@@ -22,7 +81,36 @@ async def websocket_endpoint(websocket: WebSocket):
             # send data to postgre
             query.create_coords_gps(producer_id = uid, latitude = lat, longitude = lon)
             query._conn.commit()
+
+            
+            # broadcast data to front asynchronusly
+            task = asyncio.create_task(manager.broadcast_to_front(data))
+            tasks.append(task)
+            task.add_done_callback(lambda x: tasks.remove(x))
+
+            
+
         except Exception as e:
             print(f"Error: {str(e)}")
             break
+
+    manager.disconnect(websocket)
+    
+
+@router.websocket("/ws/front")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect_front(websocket)
+    print("WS/front : Connected to websocket")
+
+    # while websocket is alive, don't disconnect
+    while websocket.client_state != 3:
+        try:
+            await manager.receive_from_consumer(websocket)
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            break
+
+    manager.disconnect(websocket)
+    print("WS/front : Disconnected from websocket")
 
